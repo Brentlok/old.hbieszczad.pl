db759b9404c57a0e7707786d2679790abf8a5a17
brentlok <brentlokk@gmail.com>
2022-05-02 11:30:51 +0200

    Final changes

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -2,49 +2,53 @@ import type { NextPage } from 'next';
 import { scrollTo } from '../utils/scrollTo';
 
 const HeroMore: NextPage = () => (
-  <div className="w-full flex flex-col items-center absolute bottom-20 md:bottom-8">
-    <p className="text-xl lg:text-3xl">Dowiedz się więcej!</p>
-    <div
-      onClick={() => scrollTo('about')}
-      className="w-11 h-24 gradient rounded-full mt-6 flex items-center justify-center relative cursor-pointer group"
-    >
-      <div className="w-5 h-5 bg-primary absolute rounded-full top-6 group-hover:translate-y-8 transition duration-300"></div>
-      <div className="w-8 h-21 bg-background lg:bg-background-2 rounded-full"></div>
+    <div className="w-full flex flex-col items-center absolute bottom-20 md:bottom-8 animate-slideUp">
+        <p className="text-xl lg:text-3xl">Dowiedz się więcej!</p>
+        <div
+            onClick={() => scrollTo('about')}
+            className="w-11 h-24 gradient rounded-full mt-6 flex items-center justify-center relative cursor-pointer group"
+        >
+            <div className="w-5 h-5 bg-primary absolute rounded-full top-6 group-hover:translate-y-8 transition duration-300"></div>
+            <div className="w-8 h-21 bg-background lg:bg-background-2 rounded-full"></div>
+        </div>
     </div>
-  </div>
 );
 
 const HeroWave: NextPage = () => (
-  <div className="hidden lg:block absolute bottom-0 left-0 w-full overflow-hidden">
-    <svg
-      className="relative block w-wave h-96 -z-10"
-      data-name="Layer 1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1200 120"
-      preserveAspectRatio="none"
-    >
-      <path
-        d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-        className="fill-background-2"
-      ></path>
-    </svg>
-  </div>
+    <div className="hidden lg:block absolute bottom-0 left-0 w-full overflow-hidden">
+        <svg
+            className="relative block w-wave h-96 -z-10"
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+        >
+            <path
+                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+                className="fill-background-2"
+            ></path>
+        </svg>
+    </div>
 );
 
 const Hero: NextPage = () => (
-  <section
-    className="relative h-screen flex justify-center items-center"
-    id="hero"
-  >
-    <div className="w-full flex flex-col-reverse -translate-y-10 lg:flex-row px-14 justify-between">
-      <p className="w-full text-2xl text-center my-6 lg:text-left lg:my-0 lg:mt-10 lg:w-2/3 lg:text-4xl xl:text-5xl 2xl:text-6xl">
-        Zajmuję się tworzeniem aplikacji webowych i mobilnych
-      </p>
-      <img className="w-full lg:w-2/5" src="/assets/hero.svg" alt="hero" />
-    </div>
-    <HeroWave />
-    <HeroMore />
-  </section>
+    <section
+        className="relative h-screen flex justify-center items-center"
+        id="hero"
+    >
+        <div className="w-full flex flex-col-reverse -translate-y-10 lg:flex-row px-14 justify-between">
+            <p className="w-full text-2xl text-center my-6 animate-slideUp lg:text-left lg:my-0 lg:mt-10 lg:w-2/3 lg:text-4xl xl:text-5xl 2xl:text-6xl lg:animate-slideRight">
+                Zajmuję się tworzeniem aplikacji webowych i mobilnych
+            </p>
+            <img
+                className="w-full lg:w-2/5 animate-slideUp lg:animate-slideLeft"
+                src="/assets/hero.svg"
+                alt="hero"
+            />
+        </div>
+        <HeroWave />
+        <HeroMore />
+    </section>
 );
 
 export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,41 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './sections/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    colors: {
-      background: 'var(--color-background)',
-      'background-2': 'var(--color-background-2)',
-      text: 'var(--color-text)',
-      wave: 'var(---color-wave)',
-      white: '#fff',
-      black: '#000',
-      gray: '#ccc',
-      primary: '#ff385c',
-      transparent: 'rgba(0,0,0,0)',
-      dark: 'rgba(0,0,0,0.8)',
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './sections/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        colors: {
+            background: 'var(--color-background)',
+            'background-2': 'var(--color-background-2)',
+            text: 'var(--color-text)',
+            wave: 'var(---color-wave)',
+            white: '#fff',
+            black: '#000',
+            gray: '#ccc',
+            primary: '#ff385c',
+            transparent: 'rgba(0,0,0,0)',
+            dark: 'rgba(0,0,0,0.8)',
+        },
+        keyframes: {
+            slideLeft: {
+                '0%': { transform: 'translateX(200px)', opacity: 0 },
+                '100%': { transform: 'translateX(0)', opacity: 1 },
+            },
+            slideRight: {
+                '0%': { transform: 'translateX(-200px)', opacity: 0 },
+                '100%': { transform: 'translateX(0)', opacity: 1 },
+            },
+            slideUp: {
+                '0%': { transform: 'translateY(200px)', opacity: 0 },
+                '100%': { transform: 'translateY(0)', opacity: 1 },
+            },
+        },
+        animation: {
+            slideLeft: 'slideLeft 1s ease-in-out',
+            slideRight: 'slideRight 1s ease-in-out',
+            slideUp: 'slideUp 1s ease-in-out',
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
